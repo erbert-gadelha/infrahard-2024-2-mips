7eae1874b8a1234240eadb75c8861c53ffd915f0
ebgr <ebgr@g1c33.cin.ufpe.br>
2024-12-10 10:56:48 -0300

    feat: divisao implementada recursivamente, sem salvar registradores na pilha.

--- lista-1/q4.asm
+++ lista-1/q4.asm
@@ -1,23 +1,51 @@
 .data
-   break_line: .asciiz "\nvalor: "
+   t_result:	.asciiz "result: "
+   t_remainder:	.asciiz "\nremainder: "
+   break_line:	.asciiz "\n"
+   DIVIDENDO:	.word 1000000
+   DIVISOR:	.word 1
+   RESULT:	.word 0
+   REMAINDER:	.word 0
 .text
 
 main:
-   li $t0, 8162
-loop:
-   addi $t0, $t0, -1
+   lw $t0, DIVIDENDO	# dividendo = DIVIDENDO;
+   lw $t1, DIVISOR	# divisor = DIVISOR;
+   li $t2, 0		# result = 0;
+   j loop_divisao   
+   
+loop_divisao:
+   sub $t0, $t0, $t1	# dividendo-=DIVISOR;
+   addi $t2, $t2, 1   	# result++;
+   bge $t0, $t1 loop_divisao   
+end_loop_divisao:
+   sw $t0, RESULT	# RESULT = result;
+   sw $t2, REMAINDER	# REMAINDER = remainder;
    jal print_value
-   beqz $t0, end_loop
-   j loop
-     
+   j exit
+   
+   
 print_value:
+   # IMPRIME ("result: ")
    li $v0, 4
-   la $a0, break_line
+   la $a0, t_result
+   syscall
+   # IMPRIME (result)
+   move $a0, $t2
+   li $v0, 1
    syscall
    
+   # IMPRIME ("\nremainder: ")
+   li $v0, 4
+   la $a0, t_remainder
+   syscall
+   # IMPRIME (dividendo)
    move $a0, $t0
    li $v0, 1
    syscall
-   jr $ra  
    
-end_loop:
+   jr $ra
+   
+exit:
+   li $v0, 10
+   syscall
